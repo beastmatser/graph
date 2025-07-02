80a2844ef35dafe54271f04efc04fe8cd63a207d
beastmatser <79206232+beastmatser@users.noreply.github.com>
2025-07-02 21:51:49 +0200

    Add some basic tests

---
+++ tests/format_test.v
@@ -0,0 +1,56 @@
+module tests
+
+import rand
+import strings
+
+import graphs { Graph }
+
+pub fn random_graph6(n i64, edge_prob f64) string {
+	mut out := strings.new_builder(1024)
+
+	// Encode number of vertices
+	if n <= 62 {
+		out.write_byte(u8(n + 63))
+	} else if n <= 258047 {
+		out.write_byte(126) // '~'
+		out.write_byte(u8(((n >> 12) & 0x3F) + 63))
+		out.write_byte(u8(((n >> 6) & 0x3F) + 63))
+		out.write_byte(u8((n & 0x3F) + 63))
+	} else {
+		out.write_byte(126)
+		out.write_byte(126)
+		for i := 0; i < 6; i++ {
+			out.write_byte(u8(((n >> (30 - i * 6)) & 0x3F) + 63))
+		}
+	}
+
+	// Generate random upper triangle bits
+	mut bits := []bool{}
+	for i in 0 .. n {
+		for j in i + 1 .. n {
+			bits << (rand.f64() < edge_prob)
+		}
+	}
+
+	// Encode 6 bits at a time
+	for i in 0 .. (bits.len + 5) / 6 {
+		mut b := 0
+		for j in 0 .. 6 {
+			idx := i * 6 + j
+			if idx < bits.len && bits[idx] {
+				b |= 1 << (5 - j)
+			}
+		}
+		out.write_byte(u8(b + 63))
+	}
+
+	return out.str()
+}
+
+
+fn test_graph6() {
+	for i in 0 .. 100 {
+		str := random_graph6(rand.i64_in_range(1, 1000) or { 100 }, i/100)
+		assert Graph.from_graph6(str).to_graph6() == str
+	}
+}
---
+++ tests/operators_test.v
@@ -0,0 +1,37 @@
+module tests
+
+import graphs { Graph, Node, cycle_graph, complete_graph, path_graph }
+
+
+// Check complement involution and structure
+fn test_complement() {
+	complete7 := complete_graph(7)
+	cycle6 := cycle_graph(6)
+	path5 := path_graph(5)
+	empty := Graph[int]{ nodes: [&Node{0}, &Node{1}, &Node{2}, &Node{3}] }
+
+	assert complete7.edges.len == complete7.complement().complement().edges.len
+	assert complete7.nodes.len == complete7.complement().complement().nodes.len
+
+	assert cycle6.nodes.len == cycle6.complement().complement().nodes.len
+	assert path5.edges.len == path5.complement().complement().edges.len
+
+	complement_empty := empty.complement()
+	assert complement_empty.edges.len == (4 * (4 - 1)) / 2
+	assert complement_empty.nodes == empty.nodes
+	assert complement_empty.complement().edges.len == 0
+}
+
+// Check line graph construction
+fn test_line_graph() {
+	triangle := complete_graph(3)
+	line := triangle.line_graph()
+
+	assert line.nodes.len == 3
+	assert line.edges.len == 3
+
+	p := path_graph(4)
+	lp := p.line_graph()
+	assert lp.nodes.len == 3
+	assert lp.edges.len == 2
+}
