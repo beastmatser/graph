b5bdf5ac342f15b71e75c0d89f1c4cfef3ed4216
beastmatser <79206232+beastmatser@users.noreply.github.com>
2025-07-09 23:31:27 +0200

    Add node_to_index field of UndirectedGraph

--- undirected/graph.v
+++ undirected/graph.v
@@ -3,13 +3,16 @@ module undirected
 import common { Edge, Node }
 
 // A graph is a list of references to nodes and a list of references to edges made up of these nodes.
-// In addition it holds an adjacency mapping, the keys are the nodes.
+// In addition, it holds an adjacency mapping, the keys are the nodes.
 // The values are maps where its keys are nodes adjacent to the original node with value
 // the weight of the edge between these adjacent nodes.
+// The field degrees maps the index of a node in the nodes list to the degree of that node.
+// Lastly, a mapping from the references of nodes to its index in the nodes list.
 pub struct UndirectedGraph[T] {
 	common.Graph[T]
 	adjacency map[int]map[int]int
 	degrees   map[int]int
+	node_to_index map[voidptr]int
 }
 
 // Factory function to create an UndirectedGraph from a list of nodes
@@ -18,24 +21,31 @@ pub fn UndirectedGraph.create[T](nodes []&Node[T], edges []&Edge[T]) UndirectedG
 	mut adj := map[int]map[int]int{}
 	mut degrees := map[int]int{}
 
-	mut node_to_int := map[voidptr]int{}
+	mut node_to_index := map[voidptr]int{}
 	for i, node in nodes {
-		node_to_int[node] = i
+		node_to_index[node] = i
 		adj[i] = {}
 		degrees[i] = 0
 	}
 
 	for edge in edges {
-		adj[node_to_int[edge.node1]][node_to_int[edge.node2]] = edge.weight
-		adj[node_to_int[edge.node2]][node_to_int[edge.node1]] = edge.weight
-		degrees[node_to_int[edge.node1]] += 1
-		degrees[node_to_int[edge.node2]] += 1
+		adj[node_to_index[edge.node1]][node_to_index[edge.node2]] = edge.weight
+		adj[node_to_index[edge.node2]][node_to_index[edge.node1]] = edge.weight
+		degrees[node_to_index[edge.node1]] += 1
+		degrees[node_to_index[edge.node2]] += 1
 	}
 
-	return UndirectedGraph[T]{common.Graph[T]{nodes, edges}, adj, degrees}
+	return UndirectedGraph[T]{common.Graph[T]{nodes, edges}, adj, degrees, node_to_index}
 }
 
 // Creates a clone of the graph, changes made in a clone will not affect the original graph.
 pub fn (graph UndirectedGraph[T]) clone[T]() UndirectedGraph[T] {
-	return UndirectedGraph[T]{graph.Graph.clone(), graph.adjacency, graph.degrees}
+	clone := graph.Graph.clone()
+	mut node_to_index := map[voidptr]int
+
+	for i, node in clone.nodes {
+		node_to_index[node] = i
+	}
+
+	return UndirectedGraph[T]{clone, graph.adjacency, graph.degrees, node_to_index}
 }
